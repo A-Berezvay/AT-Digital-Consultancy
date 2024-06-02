be824baa3a173923fedbfce285b189e7aba4b5b5
A-Berezvay <berezvay.albert@gmail.com>
2024-06-02 14:41:24 +0200

    Refactor header navigation - semi-final version;
    - Completed the semi-final refactoring of the header navigation.
    - Updated main navigation buttons: "Web Development", "Digital Transformation", and "Software Solutions".
    - Removed the "Service" button and integrated relevant sections into main navigation.
    - Adjusted the layout and styling for better usability and visual consistency.
    - Note: Minor changes and additional functionalities may still be applied in future updates.

--- client/public/src/global.js
+++ client/public/src/global.js
@@ -1,3 +1,145 @@
+/* === HEADER === */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    const menuIcon = document.getElementById('menu-bars');
+    const menuCloseIcon = document.getElementById('menu-close');
+    const mainMenu = document.getElementById('mainMenu');
+    const body = document.body;
+
+    const webDevLink = document.getElementById('webDevLink');
+    const softwareSolutionsLink = document.getElementById('softwareSolutionsLink');
+    const webDevDropdown = document.getElementById('webDevDropdown');
+    const softwareSolutionsDropdown = document.getElementById('softwareSolutionsDropdown');
+
+    // Functions to toggle the main menu
+    function toggleMenu(open) {
+        if (open) {
+            mainMenu.style.display = 'flex';
+            menuCloseIcon.style.display = 'block';
+            menuIcon.style.display = 'none';
+            body.style.overflow = 'hidden';
+        } else {
+            mainMenu.style.display = 'none';
+            menuCloseIcon.style.display = 'none';
+            menuIcon.style.display = 'block';
+            body.style.overflow = 'auto';
+        }
+    }
+
+    // Event listeners for the menu icons
+    menuIcon.addEventListener('click', () => toggleMenu(true));
+    menuCloseIcon.addEventListener('click', () => toggleMenu(false));
+
+
+    // Function to disable specific links
+    function disableLink(event) {
+        event.preventDefault();
+        console.log("Link disabled:", event.target);
+    }
+
+    // Function to toggle dropdown menus
+    function toggleDropdown(dropdown) {
+        if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+            dropdown.style.display = 'block';
+            console.log("Dropdown opened:", dropdown);
+        } else {
+            dropdown.style.display = 'none';
+            console.log("Dropdown closed:", dropdown);
+        }
+    }
+
+    // Functions to show and hide dropdowns on hover
+    function showDropdown(dropdown) {
+        //clearTimeout(hideDropdownTimeout);
+        dropdown.style.display = 'block';
+        console.log("Dropdown shown:", dropdown);
+    }
+    
+    function hideDropdown(dropdown) {
+        //hideDropdownTimeout = setTimeout(() => {
+            dropdown.style.display = 'none';
+            console.log("Dropdown hidden:", dropdown);
+        //}, 200);
+    }
+
+    // Update event listeners based on screen size
+    function updateEventListeners() {
+        const isMobile = window.matchMedia('(max-width: 1280px)').matches;
+
+        // Debugging logs
+        console.log("isMobile:", isMobile);
+        console.log("webDevLink:", webDevLink);
+        console.log("softwareSolutionsLink:", softwareSolutionsLink);
+
+        if (webDevLink && softwareSolutionsLink) {
+            if (isMobile) {
+                // Disable the links
+                webDevLink.addEventListener('click', disableLink);
+                softwareSolutionsLink.addEventListener('click', disableLink);
+                webDevLink.addEventListener('click', () => toggleDropdown(webDevDropdown));
+                softwareSolutionsLink.addEventListener('click', () => toggleDropdown(softwareSolutionsDropdown));
+                console.log("Event listeners added to disable links and toggle dropdowns for mobile view");
+
+                // Remove hover listeners
+                webDevLink.removeEventListener('mouseenter', showDropdownHandler);
+                webDevLink.removeEventListener('mouseleave', hideDropdownHandler);
+                webDevDropdown.removeEventListener('mouseenter', showDropdownHandler);
+                webDevDropdown.removeEventListener('mouseleave', hideDropdownHandler);
+                softwareSolutionsLink.removeEventListener('mouseenter', showDropdownHandler);
+                softwareSolutionsLink.removeEventListener('mouseleave', hideDropdownHandler);
+                softwareSolutionsDropdown.removeEventListener('mouseenter', showDropdownHandler);
+                softwareSolutionsDropdown.removeEventListener('mouseleave', hideDropdownHandler);
+            } else {
+                // Re-enable the links (remove the event listener)
+                webDevLink.removeEventListener('click', disableLink);
+                softwareSolutionsLink.removeEventListener('click', disableLink);
+                webDevLink.removeEventListener('click', () => toggleDropdown(webDevDropdown));
+                softwareSolutionsLink.removeEventListener('click', () => toggleDropdown(softwareSolutionsDropdown));
+                webDevDropdown.style.display = 'none';
+                softwareSolutionsDropdown.style.display = 'none';
+                console.log("Event listeners removed for desktop view");
+
+                //Add hover listeners for desktop view
+                webDevLink.addEventListener('mouseenter', () => showDropdown(webDevDropdown));
+                webDevLink.addEventListener('mouseleave', () => hideDropdown(webDevDropdown));
+                webDevDropdown.addEventListener('mouseenter', showDropdown);
+                webDevDropdown.addEventListener('mouseleave', hideDropdown);
+                softwareSolutionsLink.addEventListener('mouseenter', () => showDropdown(softwareSolutionsDropdown));
+                softwareSolutionsLink.addEventListener('mouseleave', () => hideDropdown(softwareSolutionsDropdown));
+                softwareSolutionsDropdown.addEventListener('mouseenter', showDropdownHandler);
+                softwareSolutionsDropdown.addEventListener('mouseleave', hideDropdownHandler);
+                console.log("Hover event listeners added for desktop view");
+            }
+        } else {
+            console.log("One or both elements not found");
+        }
+    }
+
+        // Event handlers for showing and hiding dropdowns
+        function showDropdownHandler(event) {
+            const dropdown = event.currentTarget.querySelector('.drop-down');
+            if (dropdown) {
+                showDropdown(dropdown);
+            }
+        }
+
+        function hideDropdownHandler(event) {
+            const dropdown = event.currentTarget.querySelector('.drop-down');
+            if (dropdown) {
+                hideDropdown(dropdown);
+            }
+        }
+    
+
+    // Initial setup
+    updateEventListeners();
+
+    // Update event listeners on window resize
+    window.addEventListener('resize', updateEventListeners);
+
+});
+
 
 
 /* === CONTACT FORM === */
@@ -12,8 +154,3 @@ form.addEventListener('submit', e => {
     .then(() => { window.location.reload(); })
     .catch(error => console.error('Error!', error.message))
 })
-
-
-
-
-
