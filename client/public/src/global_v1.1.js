e41fa4b4e4b97626ed0be8da40cd5ec96d5dcf8c
A-Berezvay <berezvay.albert@gmail.com>
2024-06-21 14:49:52 +0200

    Made the header functional by implementing interactive navigation;
    - Added JavaScript to handle the opening and closing of the navigation menu on mobile
    - Ensured responsive design for better user experience on mobile devices
    - Updated header links to improve navigation flow

--- client/public/src/global_v1.1.js
+++ client/public/src/global_v1.1.js
@@ -7,10 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.getElementById('nav-menu');
     const webDevNavLink = document.getElementById('webdev-navlink');
     const softwareNavLink = document.getElementById('software-navlink');
+    const marketingNavLink = document.getElementById('marketing-navlink');
     const dropdownMenuWebDev = document.getElementById('dropdown-display-webdev');
     const dropdownMenuSoftware = document.getElementById('dropdown-display-software');
+    const dropdownMenuMarketing = document.getElementById('dropdown-display-marketing');
 
-    console.log("Elements:", { menuIcon, menuCloseIcon, navMenu, navToggle, webDevNavLink, softwareNavLink, dropdownMenuWebDev, dropdownMenuSoftware });
+    console.log("Elements:", { menuIcon, menuCloseIcon, navMenu, navToggle, webDevNavLink, softwareNavLink, dropdownMenuWebDev, dropdownMenuSoftware, dropdownMenuMarketing });
 
     menuCloseIcon.style.display = 'none'; // Initial display of Close icon
 
@@ -34,9 +36,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (window.innerWidth < 1118) {
             dropdownMenuWebDev.style.display = 'none';
             dropdownMenuSoftware.style.display = 'none';
+            dropdownMenuMarketing.style.display = 'none';
         } else {
             dropdownMenuWebDev.style.display = 'block';
             dropdownMenuSoftware.style.display = 'block';
+            dropdownMenuMarketing.style.display = 'block';
         }
     }
 
@@ -53,13 +57,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     webDevNavLink.addEventListener('click', () => toggleDropdown(dropdownMenuWebDev));
     softwareNavLink.addEventListener('click', () => toggleDropdown(dropdownMenuSoftware));
+    marketingNavLink.addEventListener('click', () => toggleDropdown(dropdownMenuMarketing));
 
 
 });
 
 
 
-/* === CONTACT FORM === */
+/* === CONTACT FORM === 
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwQO0F6wiLj5AnOp8Ka1z2q9dI1zqwukCrhENTVXKOF3zTSxBVTLiqJ7Gp33hbuI7R0aA/exec'
 const form = document.forms['contact-form']
@@ -71,3 +76,5 @@ form.addEventListener('submit', e => {
     .then(() => { window.location.reload(); })
     .catch(error => console.error('Error!', error.message))
 })
+
+*/
